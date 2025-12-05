b23e8b6fca784171647630028a2758b0688585cb
imbelfort <belfort.ishmael25@outlook.com>
2025-12-05 18:40:21 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 <<<<<<< HEAD
+<<<<<<< HEAD
   base: './',
+=======
+  base: '/microsystems/',
+>>>>>>> parent of 075fbd5 (fixed)
 =======
   base: '/microsystems/',
 >>>>>>> parent of 075fbd5 (fixed)
