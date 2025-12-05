27f9186e266574a0798b0004e2578127f8b29d1a
imbelfort <belfort.ishmael25@outlook.com>
2025-12-05 18:45:23 -0400

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-<<<<<<< HEAD
-<<<<<<< HEAD
-  base: './',
-=======
-  base: '/microsystems/',
->>>>>>> parent of 075fbd5 (fixed)
-=======
-  base: '/microsystems/',
->>>>>>> parent of 075fbd5 (fixed)
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react()],
+    base: command === 'serve' ? '/' : '/microsystems/',
+  }
 })
